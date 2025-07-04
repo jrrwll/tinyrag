cd90895de85df45618fb5de18f4214bb3aa98468
tuke <jrriwll@gmail.com>
2025-07-04 22:55:27 +0800

    commit

--- app/core/workflow_run/api.py
+++ app/core/workflow_run/api.py
@@ -3,10 +3,10 @@ from uuid import UUID
 from pydantic import BaseModel
 
 
-class WorkflowR
-    unPublic(BaseModel):
+class WorkflowRunPublic(BaseModel):
     id: int
     created_at: str
+    
     updated_at: str
 
     workflow_id: int
